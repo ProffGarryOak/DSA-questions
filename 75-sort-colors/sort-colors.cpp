bc95cd93865a1d8e443b0bd926dfeb4b27b69ebd
Adarsh Pandey <158749389+ProffGarryOak@users.noreply.github.com>
2025-07-22 01:55:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.79%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z=0,o=0,t=0;
+        for (auto it:nums){
+            if (it==0) z++;
+            else if (it==1) o++;
+            else t++;
+        }
+        for (int i=0;i<z;i++){
+            nums[i]=0;
+        }
+        for (int i=z;i<o+z;i++){
+            nums[i]=1;
+        }
+        for (int i=z+o;i<t+o+z;i++){
+            nums[i]=2;
+        }
+    }
+};
